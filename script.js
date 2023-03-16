24fa433af60aa09628427376fb84063b056a9747
Scott Wright <scott.wright.uk@gmail.com>
2023-03-16 16:46:28 +0000

    feat: Add 16x16 grid using nested loops

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const container = document.getElementById('div-container');
+
+// create 16 row grid
+for (let i = 0; i < 16; i++) {
+  const row = document.createElement('div');
+  container.appendChild(row);
+
+  // create 16 column grid
+  for (let j = 0; j < 16; j++) {
+    const column = document.createElement('div');
+    container.appendChild(column);
+  }
+}
 
