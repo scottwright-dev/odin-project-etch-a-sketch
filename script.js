b7d287152bbf894e180cbff9e2c75dd98366375f
Scott Wright <scott.wright.uk@gmail.com>
2023-03-16 18:12:14 +0000

    refactor: for loops into createGrid function

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-const container = document.getElementById('div-container');
 
-// create 16 row grid
-for (let i = 0; i < 16; i++) {
-  const row = document.createElement('div');
-  container.appendChild(row);
-
-  // create 16 column grid
-  for (let j = 0; j < 16; j++) {
-    const column = document.createElement('div');
-    container.appendChild(column);
+//function creates a grid with the specified number of rows and columns
+function createGrid(rows, columns) {
+    //get container element where the grid will be created
+    const container = document.getElementById('div-container');
+  
+    //create rows for the grid
+    for (let i = 0; i < rows; i++) {
+      const row = document.createElement('div');
+      container.appendChild(row);
+  
+      //create columns for each row
+      for (let j = 0; j < columns; j++) {
+        const column = document.createElement('div');
+        row.appendChild(column);
+      }
+    }
   }
-}
-
+  //call the createGrid function
+  createGrid(16, 16);
+  
