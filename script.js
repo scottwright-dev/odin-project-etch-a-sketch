7400b042f2176db5d747e2691c7330721b2ab6f9
Scott Wright <scott.wright.uk@gmail.com>
2023-03-17 08:33:07 +0000

    fix: typo causing duplicate top row in grid

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-
-//function creates a grid with the specified number of rows and columns
 function createGrid(rows, columns) {
-    //get container element where the grid will be created
-    const container = document.getElementById('div-container');
-  
-    //create rows for the grid
-    for (let i = 0; i < rows; i++) {
-      const row = document.createElement('div');
-      container.appendChild(row);
-  
-      //create columns for each row
-      for (let j = 0; j < columns; j++) {
-        const column = document.createElement('div');
-        row.appendChild(column);
-      }
+  //get container element where the grid will be created
+  const container = document.getElementById('div-container');
+
+  //create rows for the grid
+  for (let i = 0; i < rows; i++) {
+    const row = document.createElement('div');
+    container.appendChild(row);
+
+    //create columns for each row
+    for (let j = 0; j < columns; j++) {
+      const column = document.createElement('div');
+      container.appendChild(column);
     }
   }
-  //call the createGrid function
-  createGrid(16, 16);
-  
+}
+createGrid(16, 16);
