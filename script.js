386ac4b4ef382c1c8136787f775f1bacb7538b48
Scott Wright <scott.wright.uk@gmail.com>
2023-03-17 09:58:29 +0000

    refactor: createGrid function for readability

--- script.js
+++ script.js
@@ -2,16 +2,13 @@ function createGrid(rows, columns) {
   //get container element where the grid will be created
   const container = document.getElementById('div-container');
 
-  //create rows for the grid
+  //create rows and columns for the grid
   for (let i = 0; i < rows; i++) {
-    const row = document.createElement('div');
-    container.appendChild(row);
-
-    //create columns for each row
     for (let j = 0; j < columns; j++) {
-      const column = document.createElement('div');
-      container.appendChild(column);
+      const square = document.createElement('div');
+      container.appendChild(square);
     }
   }
 }
 createGrid(16, 16);
+
