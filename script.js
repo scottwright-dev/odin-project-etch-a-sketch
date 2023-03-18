b050e2021c0eab4d0de762d1621b4cbe6d208d1f
Scott Wright <scott.wright.uk@gmail.com>
2023-03-18 12:23:04 +0000

    add: user input button & addColorHover function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// --- CREATE GRID ---
 function createGrid(rows, columns) {
   //get container element where the grid will be created
   const container = document.getElementById('div-container');
+  container.innerHTML = ''; // Clear previous grid
 
   //create rows and columns for the grid
   for (let i = 0; i < rows; i++) {
@@ -9,15 +11,38 @@ function createGrid(rows, columns) {
       container.appendChild(square);
     }
   }
+
+  // Call the addColorHover function
+  addColorHover();
 }
+
+// --- ADD COLOR ON HOVER ---
+function addColorHover() {
+  const colorDiv = document.querySelectorAll('#div-container div');
+
+  colorDiv.forEach(div => {
+    div.addEventListener('mouseover', () => {
+      div.style.backgroundColor = 'black';
+    })
+  });
+}
+
+// --- NEW GRID BUTTON ---
+function generateGridSize() {
+  let gridSize = prompt('Enter desired grid size. Type a number between 1-100.');
+  while (gridSize > 100) {
+    gridSize = prompt('Your number must be less than or equal to 100:');
+  }
+  return gridSize;
+}
+
+// Initialize default grid
 createGrid(16, 16);
 
-// Get all divs inside the container to apply hover state
-const colorDiv = document.querySelectorAll('#div-container div');
+// Add event listener for the "New Grid" button
+const newGridButton = document.getElementById('gridButton');
 
-// Add event listener to each div to change background color on hover
-colorDiv.forEach(div => {
-  div.addEventListener('mouseover', () => {
-    div.style.backgroundColor = 'black';
-  })
+newGridButton.addEventListener('click', () => {
+  const gridSize = generateGridSize();
+  createGrid(gridSize, gridSize);
 });
